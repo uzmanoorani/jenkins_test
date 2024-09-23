683b67a528882f4667f78653990eea5ac4bf28b6
Eurus <uzma.bibi@eurustechnologies.com>
2024-09-23 13:44:02 +0500

    test

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-
+JOB_NAME=$1
+MESSAGE="Job \`${JOB_NAME}\` started"
 # Accept the Slack Webhook URL as the first parameter
 #SLACK_WEBHOOK_URL=$1
 
 # Accept the GitHub job name as the second parameter
-JOB_NAME=$1
+
 
 # Slack message
-MESSAGE="Job \`${JOB_NAME}\` started"
+
 
 # Send message to Slack
 #curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"${MESSAGE}\"}" $SLACK_WEBHOOK_URL
