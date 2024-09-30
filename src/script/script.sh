a3259f71a7d442a3c9629480defea475a874654a
Eurus <uzma.bibi@eurustechnologies.com>
2024-09-30 19:20:24 +0500

    test

--- src/script/script.sh
+++ src/script/script.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-SLACK_WEBHOOK_URL=$1
-JOB_NAME=$2
-CUSTOM_MESSAGE=$3
+echo "The secret is: $secret"
+SLACK_WEBHOOK_URL=$secret
+echo "The secret is: $SLACK_WEBHOOK_URL"
+JOB_NAME=$1
+CUSTOM_MESSAGE=$2
 MESSAGE="Job \`${JOB_NAME}\`: ${CUSTOM_MESSAGE}"
 echo "${message}"
-curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"${MESSAGE}\"}" $SLACK_WEBHOOK_URL
+
+#curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"${MESSAGE}\"}" $SLACK_WEBHOOK_URL
