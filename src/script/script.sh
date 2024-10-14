42f7dfa374b51f98634794393121b25d5b64852a
Eurus <uzma.bibi@eurustechnologies.com>
2024-10-14 17:15:51 +0500

    test

--- src/script/script.sh
+++ src/script/script.sh
@@ -5,7 +5,7 @@ JOB_NAME=$2
 CUSTOM_MESSAGE=$3
 Notify_slack=$4
 MESSAGE="Job \`${JOB_NAME}\`: ${CUSTOM_MESSAGE}"
-if [[ "$Notify_slack" =~ ^[0-9]+$ && "$Notify_slack" -eq 1 ]]; then
+if [ "$Notify_slack" -eq 1 ]; then
   echo "Sending Slack notification..."
   curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"${MESSAGE}\"}" $SLACK_WEBHOOK_URL
 else
