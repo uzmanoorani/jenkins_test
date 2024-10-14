1c5484dea1595bcfdeaa0965c53a6bb45c432af8
Eurus <uzma.bibi@eurustechnologies.com>
2024-10-14 17:10:31 +0500

    test

--- src/script/script.sh
+++ src/script/script.sh
@@ -3,7 +3,11 @@ SLACK_WEBHOOK_URL=$1
 echo "The secret is: $SLACK_WEBHOOK_URL"
 JOB_NAME=$2
 CUSTOM_MESSAGE=$3
+Notify_slack=$4
 MESSAGE="Job \`${JOB_NAME}\`: ${CUSTOM_MESSAGE}"
-echo "${message}"
-
-curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"${MESSAGE}\"}" $SLACK_WEBHOOK_URL
+if [ "$Notify_slack" -eq 1 ]; then
+  echo "Sending Slack notification..."
+  curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"${MESSAGE}\"}" $SLACK_WEBHOOK_URL
+else
+  echo "Slack notification is disabled (Notify_slack = $Notify_slack)."
+fi
